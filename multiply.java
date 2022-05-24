f0f828b5a70d61622b2134469e88fce84b272075
Shubham Patel <shubhampatel196@gmail.com>
2022-05-24 15:34:35 +0530

    Multiplication of two numbers

---
+++ multiply.java
@@ -0,0 +1,12 @@
+public class multiply {
+   
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 5;
+
+        int mul = a*b;
+
+        System.out.println(mul);
+    }
+}
