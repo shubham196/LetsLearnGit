9d5210461ba8dcadf96f3ee8c64d0c414e14535b
Shubham Patel <shubhampatel196@gmail.com>
2022-05-24 14:56:09 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add 
+{
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 6;
+
+        int add = a+b;
+
+        System.out.println(add);
+    }
+}
